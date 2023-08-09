780896f5991a864ccbd8d4006c0fed41f813c9cf
samuelbamueldamuel <samdinardi@gmail.com>
2023-08-09 17:38:12 -0400

    smth

--- ball_game/settings.py
+++ ball_game/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'game',
         'USER': 'root',
-        'PASSWORD': '18261826',
+        'PASSWORD': 'password',
         'HOST':  '127.0.0.1',
         'PORT': '3306'
     }
